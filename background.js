2e432cf12c59e5deef59857f943973f2983120ac
Sjors van Holst <sjorsvanholst@gmail.com>
2022-04-25 21:52:46 +0200

    ♻️ Changed default interval

--- background.js
+++ background.js
@@ -2,6 +2,6 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ ticketmain_min: 0.00 });
   chrome.storage.sync.set({ ticketmain_max: 100.00 });
   chrome.storage.sync.set({ ticketmain_amount: 1.0 });
-  chrome.storage.sync.set({ ticketmain_interval: 1000 });
+  chrome.storage.sync.set({ ticketmain_interval: 5000 });
   chrome.storage.sync.set({ ticketmain_disabled: true });
 });
