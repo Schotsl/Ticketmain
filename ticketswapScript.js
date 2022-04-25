2c96418f03d66dda871b07d2df3549f89f23eeb3
Sjors van Holst <sjorsvanholst@gmail.com>
2022-04-25 21:25:46 +0200

    🎨 Formatted codebase

--- ticketswapScript.js
+++ ticketswapScript.js
@@ -1,23 +1,23 @@
 function getTickets() {
   const availableTicketsField = document.querySelector(
     "[data-testid='available-tickets-list']"
-    );
-    
-    if (!availableTicketsField) {
-      return [];
-    }
-    
-    const liItems = availableTicketsField.getElementsByTagName("li");
-    const tickets = [];
+  );
 
-    for (let i = 0; i < liItems.length; i++) {
-      const ticket = liItems[i];
-      const ticketAmount = parseInt(ticket.getElementsByTagName("h4")[0].innerText.split(" ")[0]);
-      const ticketPrice = ticket.getElementsByTagName("strong")[0].innerText.split("\n")[0];
-      tickets.push({htmlElement: ticket, ticketAmount, ticketPrice});
-    }
+  if (!availableTicketsField) {
+    return [];
+  }
 
-    return tickets;
+  const liItems = availableTicketsField.getElementsByTagName("li");
+  const tickets = [];
+
+  for (let i = 0; i < liItems.length; i++) {
+    const ticket = liItems[i];
+    const ticketAmount = parseInt(ticket.getElementsByTagName("h4")[0].innerText.split(" ")[0]);
+    const ticketPrice = ticket.getElementsByTagName("strong")[0].innerText.split("\n")[0];
+    tickets.push({ htmlElement: ticket, ticketAmount, ticketPrice });
+  }
+
+  return tickets;
 }
 
 function selectTicket(ticket) {
@@ -26,7 +26,7 @@ function selectTicket(ticket) {
 
 function findBuyTicketButton() {
   const buttons = document.getElementsByTagName("button");
-  
+
   for (let i = 0; i < buttons.length; i++) {
     const button = buttons[i];
     if (button.innerText === "Buy ticket") {
@@ -48,7 +48,7 @@ function checkForTickets() {
   // Get options out of storage
   let minPrice, maxPrice, amount, interval, disabled;
 
-  chrome.storage.sync.get(["minPrice", "maxPrice", "amount", "interval", "disabled"], function(items) {
+  chrome.storage.sync.get(["minPrice", "maxPrice", "amount", "interval", "disabled"], function (items) {
     minPrice = items.minPrice || 0;
     maxPrice = items.maxPrice || 999999;
     amount = items.amount || 0;
@@ -72,7 +72,7 @@ function checkForTickets() {
     reloadPage(interval);
     return
   }
-  
+
   // Check all the available tickets
   for (let i = 0; i < tickets.length; i++) {
     const ticket = tickets[i];
@@ -81,7 +81,7 @@ function checkForTickets() {
     if ((ticket.ticketAmount == amount || amount == 0) && ticket.ticketPrice >= minPrice && ticket.ticketPrice <= maxPrice) {
       console.log("Matching ticket found!");
       selectTicket(ticket);
-      
+
       // Wait for new page load
       setTimeout(findBuyTicketButton().click, 3000);
       break;
@@ -97,7 +97,7 @@ function checkForTickets() {
 console.log("TicketSwap detected! Running script...");
 let disabled = false;
 
-chrome.storage.sync.get(["disabled"], function(items) {
+chrome.storage.sync.get(["disabled"], function (items) {
   disabled = items.disabled || false;
 });
 
