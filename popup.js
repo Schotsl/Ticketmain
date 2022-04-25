2f89cc2d25a309c0c06f148cea5710cba1f7883e
Sjors van Holst <sjorsvanholst@gmail.com>
2022-04-25 20:35:46 +0200

    👽️ Added Chome storage API

--- popup.js
+++ popup.js
@@ -6,4 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
     img.classList.toggle('disabled');
     body.classList.toggle('disabled');
   });
+
+  const minElement = document.getElementById("ticketmain_min");
+  const maxElement = document.getElementById("ticketmain_max");
+  const amountElement = document.getElementById("ticketmain_amount");
+  const intervalElement = document.getElementById("ticketmain_interval");
+
+  minElement.addEventListener('input', () => chrome.storage.sync.set({ ticketmain_min: minElement.value }));
+  maxElement.addEventListener('input', () => chrome.storage.sync.set({ ticketmain_max: minElement.value }));
+  amountElement.addEventListener('input', () => chrome.storage.sync.set({ ticketmain_amount: minElement.value }));
+  intervalElement.addEventListener('input', () => chrome.storage.sync.set({ ticketmain_interval: minElement.value }));
 });
