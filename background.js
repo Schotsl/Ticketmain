f80c7b6dc620acfd07cfea745f5979ec4d46ec79
Sjors van Holst <sjorsvanholst@gmail.com>
2022-05-08 13:04:47 +0200

    ♻️ Refactored Javascript

--- background.js
+++ background.js
@@ -21,9 +21,10 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ ticketmain_advanced_interval: 5.0 });
 
   chrome.storage.sync.set({ ticketmain_dropdown_disabled: true });
-  chrome.storage.sync.set({ ticketmain_dropdowns_price: false });
-  chrome.storage.sync.set({ ticketmain_dropdowns_amount: true });
-  chrome.storage.sync.set({ ticketmain_dropdowns_advanced: false });
+  
+  chrome.storage.sync.set({ ticketmain_fieldset_price: false });
+  chrome.storage.sync.set({ ticketmain_fieldset_amount: true });
+  chrome.storage.sync.set({ ticketmain_fieldset_advanced: false });
 });
 
 chrome.runtime.onStartup.addListener(async () => {
--- popup.js
+++ popup.js
@@ -1,65 +1,225 @@
-function validateInput(inputElement, errorElement, minValue, maxValue) {
+function validateInput(input, error, min, max) {
   // Remove error message if it exists
-  errorElement.textContent = "";
+  error.textContent = "";
 
   // Make sure there is any input
-  if (inputElement.value === "") {
-    errorElement.textContent = "Minimum price is required";
+  if (input.value === "") {
+    error.textContent = "Minimum price is required";
     return false;
   }
 
   // Make sure the input is a number
-  if (Number.isInteger(inputElement.value)) {
-    errorElement.textContent = "Minimum price should be a number";
+  if (Number.isInteger(input.value)) {
+    error.textContent = "Minimum price should be a number";
     return false;
   }
 
   // Make sure the input is between the given parameters
-  if (inputElement.value < minValue) {
-    errorElement.textContent = "Minimum price should be equal or above 0";
+  if (parseFloat(input.value) < min) {
+    error.textContent = `Minimum price should be equal or above ${min}`;
     return false;
   }
 
-  if (inputElement.value > maxValue) {
-    errorElement.textContent = "Minimum price should be equal or below 100";
+  if (parseFloat(input.value) > max) {
+    error.textContent = `Minimum price should be equal or below ${max}`;
     return false;
   }
 
   return true;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const inputObjects = [
+
+
+
+
+
+function watchForm(inputs) {
+  const form = document.getElementById("ticketmain_form");
+
+  form.addEventListener("submit", (event) => {
+    // Make sure the form doesn't process
+    event.preventDefault();
+
+    // Validate every input
+    for (let i = 0; i < inputs.length; i++) {
+      const input = inputs[i];
+      const { target, error, min, max } = input;
+      
+      if (!validateInput(target, error, min, max)) return;
+    }
+
+    // Store every input in Chrome storage
+    for (let i = 0; i < inputs.length; i++) {
+      const input = inputs[i];
+      const { target, label } = input;
+
+      chrome.storage.sync.set({ [label]: parseFloat(target.value) });
+    }
+  });
+}
+
+async function setupForm(inputs) {
+  // Load every input from Chrome storage
+  for (i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+    const { label, target } = input;
+    const value = await chrome.storage.sync.get([label]);
+
+    target.value = value[label];
+  }
+}
+
+
+
+
+
+
+async function setupLegends() {
+  const fields = [
+    'ticketmain_fieldset_price',
+    'ticketmain_fieldset_amount',
+    'ticketmain_fieldset_advanced',
+  ];
+
+  const results = await chrome.storage.sync.get(fields);
+  const elements = [
+    document.getElementById("ticketmain_fieldset_price"),
+    document.getElementById("ticketmain_fieldset_amount"),
+    document.getElementById("ticketmain_fieldset_advanced"),
+  ]
+
+  // Set the status of the fieldsets
+  elements.forEach(element => {
+    toggleLegend(element, results[element.id]);
+  });
+}
+
+function watchLegends() {
+  const legends = document.getElementsByTagName("legend");
+
+  for (let i = 0; i < legends.length; i++) {
+    const legend = legends[i];
+    const parent = legend.parentElement;
+    
+    legend.addEventListener("click", () => toggleLegend(parent));
+  }
+}
+
+function toggleLegend(fieldset, status = null) {
+  const legend = fieldset.getElementsByTagName("legend")[0];
+  const children = fieldset.getElementsByTagName("div");
+
+  // If no status has been given we'll just toggle the fieldset
+  status = status !== null ? status : legend.classList.contains("ticketmain_closed");
+
+  // Update the storage
+  chrome.storage.sync.set({ [fieldset.id]: status });
+
+  // Update the plus / minus symbol
+  if (status) {
+    legend.classList.remove("ticketmain_closed");
+  } else {
+    legend.classList.add("ticketmain_closed");
+  }
+
+  // Hide every child div
+  for (let j = 0; j < children.length; j++) {
+    const child = children[j];
+
+    if (status) {
+      child.classList.remove("ticketmain_hidden");
+    } else {
+      child.classList.add("ticketmain_hidden");
+    }
+  }
+}
+
+
+
+
+
+
+async function setupButton() {
+  const fields = [
+    'ticketmain_dropdown_disabled',
+  ];
+
+  const results = await chrome.storage.sync.get(fields);
+  const elements = [ document.getElementById("ticketmain_toggle") ];
+
+  // Set the status of the fieldsets
+  elements.forEach(element => {
+    toggleButton(element, results[element.id]);
+  });
+}
+
+function watchButton() {
+  const button = document.getElementById("ticketmain_toggle");
+    
+  button.addEventListener("change", () => toggleButton(button.checked));
+}
+
+function toggleButton(status) {
+  const button = document.getElementById("ticketmain_toggle");
+  const inputs = document.querySelectorAll("form input");
+  const container = document.getElementById("ticketmain_container");
+
+  button.checked = status;
+
+  if (status) {
+    container.classList.remove("disabled");
+  } else {
+    container.classList.add("disabled");
+  }
+
+  for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+
+    if (status) {
+      input.removeAttribute("disabled");
+    } else {
+      input.setAttribute("disabled", true);
+    }
+  }
+}
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const inputs = [
     {
-      input: document.getElementById("ticketmain_price_min_input"),
+      target: document.getElementById("ticketmain_price_min_input"),
       error: document.getElementById("ticketmain_price_min_error"),
       label: "ticketmain_price_min",
       max: 100,
       min: 0,
     },
     {
-      input: document.getElementById("ticketmain_price_max_input"),
+      target: document.getElementById("ticketmain_price_max_input"),
       error: document.getElementById("ticketmain_price_max_error"),
       label: "ticketmain_price_max",
       max: 100,
       min: 0,
     },
     {
-      input: document.getElementById("ticketmain_amount_min_input"),
+      target: document.getElementById("ticketmain_amount_min_input"),
       error: document.getElementById("ticketmain_amount_min_error"),
       label: "ticketmain_amount_min",
       max: 100,
       min: 0,
     },
     {
-      input: document.getElementById("ticketmain_amount_max_input"),
+      target: document.getElementById("ticketmain_amount_max_input"),
       error: document.getElementById("ticketmain_amount_max_error"),
       label: "ticketmain_amount_max",
       max: 100,
       min: 0,
     },
     {
-      input: document.getElementById("ticketmain_advanced_interval_input"),
+      target: document.getElementById("ticketmain_advanced_interval_input"),
       error: document.getElementById("ticketmain_advanced_interval_error"),
       label: "ticketmain_advanced_interval",
       max: 10,
@@ -67,105 +227,11 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   ];
 
-  const popupInputs = document.querySelectorAll("form input");
-  const legendElements = document.getElementsByTagName("legend");
+  await setupLegends();
+  await setupButton();
+  await setupForm(inputs);
 
-  const formElement = document.getElementById("ticketmain_form");
-  const toggleElement = document.getElementById("ticketmain_toggle");
-  const containerElement = document.getElementById("ticketmain_container");
-
-  formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    // if (!popupEnabled) return;
-
-    // Validate every input
-    inputObjects.forEach((inputObject) => {
-      const { input, error, min, max } = inputObject;
-      return !validateInput(input, error, min, max);
-    });
-
-    // Store every input in Chrome storage
-    inputObjects.forEach((inputObject) => {
-      const { input, label } = inputObject;
-      chrome.storage.sync.set({ [label]: input.value });
-    });
-  });
-
-  chrome.storage.sync.get(["ticketmain_dropdown_disabled"], (result) => {
-    updatedDisabled(
-      result.ticketmain_dropdown_disabled,
-      containerElement,
-      popupInputs,
-      toggleElement,
-    );
-  });
-
-  // Load every input from Chrome storage
-  inputObjects.forEach((inputObject) => {
-    const { label, input } = inputObject;
-
-    chrome.storage.sync.get([label], (result) => {
-      input.value = result[label];
-    });
-  });
-
-  // Manage the dropdowns
-  for (let i = 0; i < legendElements.length; i++) {
-    const legendElement = legendElements[i];
-
-    legendElement.addEventListener("click", () => {
-      const divElements = legendElement.parentElement.getElementsByTagName(
-        "div",
-      );
-
-      legendElement.classList.toggle("ticketmain_closed");
-
-      for (let j = 0; j < divElements.length; j++) {
-        const divElement = divElements[j];
-
-        divElement.classList.toggle("ticketmain_hidden");
-      }
-
-      for (const popupInput of popupInputs) {
-        popupInput.setAttribute("disabled", true);
-      }
-    });
-  }
-
-  // Manage the toggle
-  toggleElement.addEventListener("change", () => {
-    updatedDisabled(
-      !toggleElement.checked,
-      containerElement,
-      popupInputs,
-      toggleElement,
-    );
-  });
+  watchLegends();
+  watchButton();
+  watchForm(inputs);
 });
-
-function updatedDisabled(
-  disabled,
-  containerElement,
-  popupInputs,
-  toggleElement,
-) {
-  // Update the chrome storage
-  chrome.storage.sync.set({ ticketmain_dropdown_disabled: disabled });
-
-  toggleElement.checked = !disabled;
-
-  if (disabled) {
-    containerElement.classList.add("disabled");
-
-    for (const popupInput of popupInputs) {
-      popupInput.setAttribute("disabled", true);
-    }
-  } else {
-    containerElement.classList.remove("disabled");
-
-    for (const popupInput of popupInputs) {
-      popupInput.removeAttribute("disabled");
-    }
-  }
-}
